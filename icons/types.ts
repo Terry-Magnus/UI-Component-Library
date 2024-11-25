165226e68f732a70cd13f898ecfb1e0cbfc4081e
Michael Uyanna <66229464+Terry-Magnus@users.noreply.github.com>
2024-11-25 16:03:55 +0100

    Update types.ts

--- icons/types.ts
+++ icons/types.ts
@@ -4,3 +4,5 @@ export interface TIconProps extends SvgIconProps {
   alternate?: boolean;
   customcolor?: string;
 }
+// alternate is just a switch that helps change the icon. In the case of the project,
+//the icons were two-toned. Look through the examples you'll see what i'm referring to....
